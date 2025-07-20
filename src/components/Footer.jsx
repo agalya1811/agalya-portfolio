8f0bd8391f362f436d38fcf5a3f883c3defc444d
Agalya M <akalyakannan8872@gmail.com>
2025-07-20 11:24:31 +0400

    Initial portfolio commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="text-center text-sm py-4 bg-white border-t">
+    <footer className="text-center text-sm py-4 bg-black border-t">
       © {new Date().getFullYear()} Agalya Muthukannan
     </footer>
   );
