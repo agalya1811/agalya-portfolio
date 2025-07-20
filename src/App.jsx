8750548be8d8bff63df0bd22c93270946b1ea49c
Agalya M <akalyakannan8872@gmail.com>
2025-07-20 11:28:40 +0400

    Initial portfolio commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,14 @@ export default function App() {
   return (
     <div className="font-sans text-gray-800 min-h-screen flex flex-col">
       <Header />
-      <Hero />
-      <Projects />
-      <SkillsGrid />
-      <ExperienceTimeline />
-      <Certifications />
-      <ContactSection />
+      <main className="flex-grow">
+        <Hero />
+        <Projects />
+        <SkillsGrid />
+        <ExperienceTimeline />
+        <Certifications />
+        <ContactSection />
+      </main>
       <Footer />
     </div>
   );
